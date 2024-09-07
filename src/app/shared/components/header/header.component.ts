0c1d3c8d7aa00622934a995e72272faab022e8f2
David Hofer <chassis.poppets0x@icloud.com>
2024-09-07 17:36:30 +0200

    Switch Language

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  activeLanguage: string = 'en';
+
+  constructor(public translate: TranslateService) {}
+
+  switchLanguage(language: string) {
+    this.activeLanguage = language;
+    this.translate.use(language);
+  }
 }

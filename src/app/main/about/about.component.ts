e363c1b2c4ffb47934609c413532c6e917f81901
David Hofer <chassis.poppets0x@icloud.com>
2024-09-08 12:32:25 +0200

    About Start

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/shared/components/header/popover/popover.component.ts
+++ src/app/shared/components/header/popover/popover.component.ts
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
   styleUrl: './popover.component.scss'
 })
 export class PopoverComponent {
-
 }

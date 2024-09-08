bd2ac97d56d0b7ac5603385c36fdaaf0a7882308
David Hofer <chassis.poppets0x@icloud.com>
2024-09-08 10:03:48 +0200

    Burger Menu

---
+++ src/app/shared/components/header/burger-menu/burger-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-burger-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './burger-menu.component.html',
+  styleUrl: './burger-menu.component.scss'
+})
+export class BurgerMenuComponent {
+
+}
+
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { PopoverComponent } from './popover/popover.component';
+import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule, PopoverComponent],
+  imports: [TranslateModule, PopoverComponent, BurgerMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })

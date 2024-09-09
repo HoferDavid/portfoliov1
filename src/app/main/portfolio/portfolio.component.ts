1765268bfb08fdfbc579e5b1ceee1f5b06abcca2
David Hofer <chassis.poppets0x@icloud.com>
2024-09-09 17:43:43 +0200

    Section Updates

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })

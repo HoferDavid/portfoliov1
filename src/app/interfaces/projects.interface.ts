563ec35ff8d78db23b61c41299ffafe53a500b06
David Hofer <chassis.poppets0x@icloud.com>
2024-09-10 19:55:33 +0200

    Structure Update

---
+++ src/app/interfaces/projects.interface.ts
@@ -0,0 +1,8 @@
+export interface Project {
+    name: string,
+    tech: string,
+    description: string,
+    media: string,
+    github: string,
+    website: string
+}
--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { Project } from '../../interfaces/projects.interface';
 
 @Component({
   selector: 'app-portfolio',
@@ -10,4 +11,30 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class PortfolioComponent {
 
+  projects: Project[] = [
+    {
+      name: 'Join',
+      tech: 'JavaScript | Firebase | CSS | HTML',
+      description: 'portfolio.projects.join.description',
+      media: 'assets/img/join.png',
+      github: 'https://github.com/HoferDavid/join-kanban',
+      website: 'https://davidhofer.com/join/'
+    },
+    {
+      name: 'El Pollo Loco',
+      tech: 'JavaScript | OOP | Canvas | CSS | HTML',
+      description: 'portfolio.projects.polloloco.description',
+      media: 'assets/img/epl.png',
+      github: 'https://github.com/HoferDavid/elpolloloco',
+      website: 'https://davidhofer.com/elpolloloco/'
+    },
+    {
+      name: 'PokeDex',
+      tech: 'JavaScript | Rest-Api | CSS | HTML',
+      description: 'portfolio.projects.pokedex.description',
+      media: 'assets/img/pokedex.png',
+      github: 'https://github.com/HoferDavid/pokedex',
+      website: 'https://davidhofer.com/pokedex/'
+    }
+  ]
 }

77711d8a566e16c84ba7d57d34092b4ca8cf6810
David Hofer <chassis.poppets0x@icloud.com>
2024-09-11 20:53:57 +0200

    Styles

--- src/app/shared/components/footer/contactform/contactform.component.ts
+++ src/app/shared/components/footer/contactform/contactform.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, TranslateModule],
   templateUrl: './contactform.component.html',
   styleUrl: './contactform.component.scss'
 })

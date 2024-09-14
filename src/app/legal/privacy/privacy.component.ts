ff380e61a9249f6a3c6606eedf6f4f981be70d00
David Hofer <chassis.poppets0x@icloud.com>
2024-09-14 10:10:35 +0200

    Footer Style Fix

--- src/app/legal/privacy/privacy.component.ts
+++ src/app/legal/privacy/privacy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './privacy.component.html',
   styleUrl: './../legal.scss'
 })

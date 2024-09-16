8336775f371b8d14bbf448d266dcc44e60ccbb90
David Hofer <chassis.poppets0x@icloud.com>
2024-09-16 18:48:22 +0200

    Json eng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,6 @@ export class AppComponent {
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'de']);
     translate.setDefaultLang('en');
-    translate.use('de');
+    translate.use('en');
   }
 }
